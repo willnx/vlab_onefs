ffa11a2c0c6543a9fc77e1a5478399949f9ef69f
willnx <willnx84@gmail.com>
2018-11-14 10:05:12 -0800

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(name="vlab-onefs-api",
       author="Nicholas Willhite,",
       author_email='willnx84@gmail.com',
-      version='2018.07.25',
+      version='2018.11.14',
       packages=find_packages(),
       include_package_data=True,
       package_files={'vlab_onefs_api' : ['app.ini']},
