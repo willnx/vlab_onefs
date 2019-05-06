0ea4ad046334805c8d231fa20d70cc214dcc47e8
willnx <willnx84@gmail.com>
2019-05-06 11:59:23 -0700

    vLab can now deploy OneFS 8.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(name="vlab-onefs-api",
       author="Nicholas Willhite,",
       author_email='willnx84@gmail.com',
-      version='2019.04.19',
+      version='2019.04.06',
       packages=find_packages(),
       include_package_data=True,
       package_files={'vlab_onefs_api' : ['app.ini']},
